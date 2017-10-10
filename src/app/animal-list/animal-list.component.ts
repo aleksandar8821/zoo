7ef5d142062a8615b93509df11144ef1b75ed311
Ale Ksandar <88aleksandar88@gmail.com>
2017-10-10 20:40:50 +0200

    zadatak 4

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Animal } from './classAnimal';
 
 @Component({
   selector: 'app-animal-list',
@@ -7,13 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimalListComponent implements OnInit {
 
-	list = [
-		{"name": "dog", "species": "dog", "dateOfBirth": "18.07.2011."},
-		{"name": "piggeon", "species": "bird", "dateOfBirth": "25.07.2011."},
-		{"name": "tunfish", "species": "fish", "dateOfBirth": "18.09.2015."},
-		{"name": "raven", "species": "bird", "dateOfBirth": ""}
+	// list = [
+	// 	{"name": "dog", "species": "dog", "dateOfBirth": "18.07.2011."},
+	// 	{"name": "piggeon", "species": "bird", "dateOfBirth": "25.07.2011."},
+	// 	{"name": "tunfish", "species": "fish", "dateOfBirth": "18.09.2015."},
+	// 	{"name": "raven", "species": "bird", "dateOfBirth": ""}
 		
+	// ]
+
+	list:Array<Animal> = [
+		new Animal('dog', 'dog', '18.07.2011.'),
+		new Animal('pigeon', 'bird', '25.07.2011.'),
+		new Animal('tuna', 'fish', '18.09.2015.'),
+		new Animal('raven', 'bird', '')
+
 	]
+
+
+	// let dog = new Animal;
+	// dog.name = 'dog'
+	// dog.species = 'dog'
+	// dog.dateOfBirth = 
+
+	// let pigeon = new Animal;
+	// pigeon.name = 'pigeon'
+	// pigeon.species = 'bird'
+	// pigeon.dateOfBirth = '25.07.2011.'
+
+	// let tuna = new Animal;
+	// tuna.name = 'tuna'
+	// tuna.species = 'fish'
+	// tuna.dateOfBirth = '18.09.2015.'
+
+	// let raven = new Animal;
+	// raven.name = 'raven'
+	// raven.species = 'bird'
+	// raven.dateOfBirth = '18.09.2015.'
+
 
   constructor() {
 
---
+++ src/app/animal-list/classAnimal.ts
@@ -0,0 +1,11 @@
+export class Animal{
+	name:string;
+	species:string;
+	dateOfBirth:string;
+
+	constructor(name, species, dateOfBirth){
+		this.name = name
+		this.species = species
+		this.dateOfBirth = dateOfBirth
+	}
+}
